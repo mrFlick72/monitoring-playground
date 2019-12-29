071e13869b69d6275c4446ece3f7746e35521374
Valerio Vaudi <valerio.vaudi@lastminute.com>
2019-12-29 14:38:49 +0100

    first commit

---
+++ src/main/java/it/valeriovaudi/monitoringplayground/MonitoringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package it.valeriovaudi.monitoringplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/valeriovaudi/monitoringplayground/MonitoringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package it.valeriovaudi.monitoringplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
