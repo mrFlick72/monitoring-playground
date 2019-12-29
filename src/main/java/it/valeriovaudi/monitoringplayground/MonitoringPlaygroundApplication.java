9c198b3e39816b3ec0fbb7be37dabb327385413e
Valerio Vaudi <valerio.vaudi@gmail.com>
2019-12-29 16:31:03 +0100

    first commit

--- src/main/java/it/valeriovaudi/monitoringplayground/MonitoringPlaygroundApplication.java
+++ src/main/java/it/valeriovaudi/monitoringplayground/MonitoringPlaygroundApplication.java
@@ -2,12 +2,33 @@ package it.valeriovaudi.monitoringplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class MonitoringPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitoringPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringPlaygroundApplication.class, args);
+    }
+}
 
+@Configuration
+class PlayGroundRoutes {
+
+    @Bean
+    RouterFunction<ServerResponse> routes() {
+        return route()
+                .GET("/hello/{name}", helloHandler())
+                .build();
+    }
+
+    private HandlerFunction<ServerResponse> helloHandler() {
+        return serverRequest -> ServerResponse.ok().body("Hello " + serverRequest.pathVariable("name"));
+    }
 }
